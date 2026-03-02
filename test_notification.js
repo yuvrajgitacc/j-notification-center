9db7eee20a85d2162a5279bcee56064417ea2c65
YUVRAJ <yuviraj030@gmail.com>
2026-03-02 13:11:25 +0530

    Update test script for live Render URL

--- test_notification.js
+++ test_notification.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const sendTestNotification = async () => {
   try {
-    const response = await axios.post('http://localhost:3001/api/notifications', {
+    const response = await axios.post('https://j-notification-center.onrender.com/api/notifications', {
       icon: '🚀',
       title: 'Success!',
       body: 'Your custom J Notification Center is live.',
